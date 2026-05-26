104cf23bace2e7dd1b662517733b9d4cca7223ca
mck21 <marcospalomero21@gmail.com>
2026-05-26 19:25:17 +0200

    feat: implement VPC module and configure production/staging environments with S3 backend and lockfiles

---
+++ infrastructure/modules/vpc/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  description = "Project name used as prefix for all resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
